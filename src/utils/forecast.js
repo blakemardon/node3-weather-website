6b3ebb3849b53ce16ccadc9bf68c070419b73307
Blake Mardon <bam6470@gmial.com>
2019-12-11 20:31:54 -0800

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    url = 'https://api.darksky.net/forecast/b809f5e7d852be24902053296c2edb5c/' + latitude + ',' + longitude
+    
+    request({url: url, json: true}, (error, response) => {
+        if (error) {
+            callback('could not connect to forecast server', undefined)
+        } else if (response.body.error) {
+            callback('Unable to find any results', undefined)
+        } else {
+            callback(undefined, {
+                temperature: response.body.currently.temperature,
+                precipitation: response.body.currently.precipProbability
+            })
+        }
+    })
+}
+
+module.exports = forecast
